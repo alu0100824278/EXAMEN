f0a938904a1f476aa825884213cf8fa070435359
JORGE ANTONIO <alu0100824278@A32.aulas.priv>
2014-05-31 00:48:35 +0100

    Suma de Polinomios

--- polinomios.py~
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python
-#! encoding: UTF-8
-
-def suma(p1,p2):
--- solucion.py
+++ solucion.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python
 #! encoding: UTF-8
 
-#Grados
-grado = [0,1,2,3]
-#Coeficientes
-p1 = [1,0,1,0]
-p2 = [0,1,0,1]
-p3 = []
-p3.append([0]*3)
-#Suma
-for i in range (3):
-  p3[i] = p1[i] + p2[i]
-print p3
+import modulo
+
+n = int(raw_input('Grado mayor del polinomio 1: '))
+m = int(raw_input('Grado mayor del polinomio 2: '))
+
+p1 = [0]*(n+1)
+p2 = [0]*(m+1)
+
+for i in range(n+1):
+  p1[i] = int(raw_input('Coeficiente de grado (%d) (polinomio 1): ' % (i))) 
+
+for j in range(m+1):
+  p2[j] = int(raw_input('Coeficiente de grado (%d) (polinomio 2): ' % (j))) 
+
+modulo.suma(p1,p2)
